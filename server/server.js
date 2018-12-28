80d88cb3723e6801c3bb2c1ebb9266f4a83f2ee6
Andrew Sleigh <andrewsleigh@akauk.com>
2018-12-28 12:46:54 +0000

    Add GET todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,15 @@ app.post('/todos', (req, res) => {
 })
 
 // GET /todos - will get all todos
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e)
+    })
+})
+
+
 app.listen(3000, () => {
     console.log('Started on port 3000')
 })
